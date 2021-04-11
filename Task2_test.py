280bb38e4fd0e157218f9302936419538f30d1a8
BezuglyR <81791887+BezuglyR@users.noreply.github.com>
2021-04-11 22:00:47 +0300

    Update Task2_test.py

--- Task2_test.py
+++ Task2_test.py
@@ -22,8 +22,8 @@ password = 'ababagalamaga87'
 
 class Task2Case(unittest.TestCase):
     def setUp(self):
-        driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub", DesiredCapabilities.CHROME)
-        #self.driver = webdriver.Chrome()
+        #driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub", DesiredCapabilities.CHROME)
+        self.driver = webdriver.Chrome()
      
     def tearDown(self):
         self.driver.close()
