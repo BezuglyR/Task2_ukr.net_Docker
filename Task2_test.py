84b07e6c3e5d9ca130e2d6c769d8823ade28e204
BezuglyR <81791887+BezuglyR@users.noreply.github.com>
2021-04-11 21:29:08 +0300

    Update Task2_test.py

--- Task2_test.py
+++ Task2_test.py
@@ -18,40 +18,40 @@ import string
 login = 'romiktest'
 password = 'ababagalamaga87'
 
+
 class Task2Case(unittest.TestCase):
     def setUp(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--window-size=1420,1080')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        #chrome_options.add_argument("--headless")
+        #chrome_options.add_argument('--no-sandbox')
+        self.driver = webdriver.Chrome('http://localhost:4444/wd/hub', options=chrome_options)
+        #self.driver = webdriver.Chrome()
 
     def tearDown(self):
         self.driver.close()
 
     def test_mail(self):
         driver = self.driver
-
-    # Step 1 --- Login mail
+        driver.implicitly_wait(15)
+        # Step 1 --- Login mail
         driver.get('https://accounts.ukr.net/')
-        #self.assertIn('Mail @ ukr.net', driver.title)  # Check if page opened
+        # self.assertIn('Mail @ ukr.net', driver.title)  # Check if page opened
         driver.find_element_by_xpath('//body//button[3]/span[2]').click()  # Switch to English language
         driver.find_element_by_xpath('//form/div//input').send_keys(login)  # Input login
         driver.find_element_by_xpath('//form/div[2]//input').send_keys(password)  # Input password
         driver.find_element_by_xpath('//form/div[3]//input').click()  # Click to check "Public computer"
         driver.find_element_by_xpath('//form/button').click()  # Click next button
         sleep(3)
-        #self.assertIn(login + '@ukr.net', driver.title) # Check if log in
+        # self.assertIn(login + '@ukr.net', driver.title) # Check if log in
 
-    # Step 2 --- Sending mails self
-        inbox = len(driver.find_elements_by_xpath('//div[@class="screen__content"]//tbody/tr/td[4]/a')) # Inbox quantity
-        driver.find_element_by_xpath('//aside/button').click()
+        # Step 2 --- Sending mails self
+        inbox = len(driver.find_elements_by_xpath('//div[@class="screen__content"]//tbody/tr/td[4]/a'))  # Inbox quantity
+        driver.find_element_by_xpath('//div[@id="content"]/aside/button').click()
         for a in range(3):  # Inserting data by loop 3 mails
             # Creating random values
             random1 = ''.join(random.choice(string.letters.lower() + string.digits * 3) for i in range(10))
             random2 = ''.join(random.choice(string.letters.lower() + string.digits * 3) for i in range(10))
-            self.assertNotEquals(random1, random2) # Check if random not equals
+            self.assertNotEquals(random1, random2)  # Check if random not equals
 
             # Input "To", "Subject", "Massage text" and click send in a 3 mails loop
             driver.find_element_by_xpath('//section[@class="sendmsg__form"]/div/div[4]/input[2]') \
@@ -74,22 +74,22 @@ class Task2Case(unittest.TestCase):
                 except Exception as e:
                     continue
 
-    # Step 3 --- If all sent mails received
+        # Step 3 --- If all sent mails received
         # In step 2, first of all we check inbox mails quantity before send. On this step we check difference after send.
         new_inbox = len(driver.find_elements_by_xpath('//div[@class="screen__content"]//tbody/tr/td[4]/a'))
         self.assertEqual(new_inbox - 3, inbox)  # new check inbox minus mails to send must be equal to first check inbox
 
-    # Step 4 --- Collect mails data from main page
+        # Step 4 --- Collect mails data from main page
         dic = {}  # Create new blank dictionary
-        mails = driver.find_elements_by_xpath('//div[@class="screen__content"]//tbody/tr/td[4]/a')  # mails subject, text
+        mails = driver.find_elements_by_xpath(
+            '//div[@class="screen__content"]//tbody/tr/td[4]/a')  # mails subject, text
         for i in mails:  # Add data to dictionary by looping over mails
             t = (i.text).split("  ")  # Split string into 2 separated data
             dic[t[0]] = t[1]  # Adding separated key and value to dictionary
-            if(len(dic) == 3):  # If dictionary length become 3 then break loop
+            if (len(dic) == 3):  # If dictionary length become 3 then break loop
                 break
 
-    # Step 5 --- Send last mail with collected data from sent before
-        sleep(1)
+        # Step 5 --- Send last mail with collected data from sent before
         driver.find_element_by_xpath('//aside/button').click()  # New mail button
         driver.find_element_by_xpath('//section[@class="sendmsg__form"]/div/div[4]/input[2]') \
             .send_keys(login + '@ukr.net')  # Input To
@@ -98,17 +98,20 @@ class Task2Case(unittest.TestCase):
             digits = ''.join([n for n in v if n.isdigit()])  # Take digits from string
             letters = ''.join([i for i in v if i.islower()])  # Take letters from string
             text = 'Received mail on theme {} with message: {}. It contains {} letters and {} numbers.'.format(k, v,
-                                                                                         len(letters), len(digits))
+                                                                                                               len(
+                                                                                                                   letters),
+                                                                                                               len(
+                                                                                                                   digits))
             driver.find_element_by_xpath('//*[@class="mce-edit-area mce-container mce-panel mce-stack-layout-item'
-                                        ' mce-last"]/iframe').send_keys(text + '\n')  # Input text in massage box
+                                         ' mce-last"]/iframe').send_keys(text + '\n')  # Input text in massage box
         driver.find_element_by_xpath('//div[@class="controls"]/button').click()  # Send mail
-        sleep(3)
         driver.find_element_by_xpath('//div[@class="sendmsg__ads-ready"]/button[@class="action"]').click()
-    # Step 6 --- Delete all mails except last one
+        # Step 6 --- Delete all mails except last one
         driver.find_element_by_xpath('//div[@id="msglist"]//div/label').click()  # Click on check all email
         driver.find_element_by_xpath('//div[@id="msglist"]/div[2]//tbody/tr/td').click()  # Click to uncheck last mail
         driver.find_element_by_xpath('//div[@id="msglist"]/div/div/div[2]/a[2]').click()  # Click delete
-        sleep(3)
-        
+        sleep(1)
+
+
 if __name__ == '__main__':
     unittest.main()
