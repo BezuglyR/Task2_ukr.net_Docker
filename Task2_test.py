097c8c70620cd193e509cdd44f320a992c27cc77
BezuglyR <81791887+BezuglyR@users.noreply.github.com>
2021-04-12 10:39:19 +0300

    Update Task2_test.py

--- Task2_test.py
+++ Task2_test.py
@@ -48,15 +48,14 @@ class Task2Case(unittest.TestCase):
 
         # Step 2 --- Sending mails self
         inbox = len(driver.find_elements_by_xpath('//div[@class="screen__content"]//tbody/tr/td[4]/a'))  # Inbox quantity
-        sleep(5)
         driver.find_element_by_xpath('//div[@id="content"]/aside/button').click()
-        for a in range(3):  # Inserting data by loop 3 mails
+        for a in range(15):  # Inserting data by loop 15 mails
             # Creating random values
             random1 = ''.join(random.choice(string.letters.lower() + string.digits * 3) for i in range(10))
             random2 = ''.join(random.choice(string.letters.lower() + string.digits * 3) for i in range(10))
             self.assertNotEquals(random1, random2)  # Check if random not equals
 
-            # Input "To", "Subject", "Massage text" and click send in a 3 mails loop
+            # Input "To", "Subject", "Massage text" and click send in a 15 mails loop
             driver.find_element_by_xpath('//section[@class="sendmsg__form"]/div/div[4]/input[2]') \
                 .send_keys(login + '@ukr.net')
             driver.find_element_by_xpath('//section[@class="sendmsg__form"]/div[4]/div[2]/input').send_keys(random1)
@@ -67,7 +66,7 @@ class Task2Case(unittest.TestCase):
             # In loop click new massage button until last send, after last we click button to return on main mail page
             while (True):
                 try:
-                    if ((3 - (a + 1)) != 0):
+                    if ((15 - (a + 1)) != 0):
                         driver.find_element_by_xpath('//div[@class="sendmsg__ads-ready"]/button').click()
                         break
                     else:
@@ -80,8 +79,8 @@ class Task2Case(unittest.TestCase):
         # Step 3 --- If all sent mails received
         # In step 2, first of all we check inbox mails quantity before send. On this step we check difference after send.
         new_inbox = len(driver.find_elements_by_xpath('//div[@class="screen__content"]//tbody/tr/td[4]/a'))
-        sleep(1)
-        self.assertEqual(new_inbox - 3, inbox)  # new check inbox minus mails to send must be equal to first check inbox
+        sleep(3)
+        self.assertEqual(new_inbox - 15, inbox)  # new check inbox minus mails to send must be equal to first check inbox
 
         # Step 4 --- Collect mails data from main page
         dic = {}  # Create new blank dictionary
@@ -90,7 +89,7 @@ class Task2Case(unittest.TestCase):
         for i in mails:  # Add data to dictionary by looping over mails
             t = (i.text).split("  ")  # Split string into 2 separated data
             dic[t[0]] = t[1]  # Adding separated key and value to dictionary
-            if (len(dic) == 3):  # If dictionary length become 3 then break loop
+            if (len(dic) == 15):  # If dictionary length become 15 then break loop
                 break
 
         # Step 5 --- Send last mail with collected data from sent before
